414302bbeb763004e4fd6a5bf39e722db1f3d7c5
vantunes <valerio.as@gmail.com>
2021-06-10 15:50:38 +0100

    parsing cmds

--- mini_shell.c
+++ mini_shell.c
@@ -63,15 +63,20 @@ void execArgs(char** parsed)
 	// Forking a child
 	pid_t pid = fork();
 
-	if (pid == -1) {
+	if (pid == -1) 
+	{
 		printf("\nFailed forking child..");
 		return;
-	} else if (pid == 0) {
-		if (execvp(parsed[0], parsed) < 0) {
+	} 
+	else if (pid == 0) 
+	{
+		if (execvp(parsed[0], parsed) < 0)
+		{
 			printf("\nCould not execute command..");
 		}
 		exit(0);
-	} else {
+	} 
+	else {
 		// waiting for child to terminate
 		wait(NULL);
 		return;
--- shell.c
+++ shell.c
@@ -1,14 +1,39 @@
 #include "mini_shell.h"
 
+int exce_args(char **cmds)
+{
+    pid_t pid;
+    char **paths;
+    int i = 0;
+    int j = 0;
+
+    paths = ft_split(getenv("PATH"), ':');
+    pid = fork();
+    if (pid == -1)
+        perror("fork");
+    else if (pid == 0)
+    {
+        while (paths[i])
+        {
+            paths[i] = ft_strjoin(paths[i], "/");
+            paths[i] = ft_strjoin(paths[i], cmds[0]);
+            execve(paths[i], cmds,  NULL);
+            i++;
+        }
+        exit(0);
+    }
+    wait(NULL);
+}
+
 int take_line(char *str)
 {
     char *buff;
 
     buff = readline("\033[3;32mMiniShell:");
-    if (strlen(buff) != 0)
+    if (ft_strlen(buff) != 0)
     {
         add_history(buff);
-        strcpy(str, buff);
+        ft_memcpy(str, buff , ft_strlen(buff));
         return (0);
     }
     else
@@ -19,7 +44,7 @@ char **parse_cmds(char *str)
 {
     char **cmds;
 
-    str = ft_strtrim(str, "\t");
+    str = ft_strtrim(str, "\t ");
     cmds = ft_split(str, ' ');
     return (cmds);
 }
@@ -28,8 +53,20 @@ int main()
 {
     char inputString[200];
     char **cmds;
+    char **pipes;
+    int i = 0; 
 
-    take_line(inputString);       
-    cmds = parse_cmds(inputString);
-    printf("%s\n",cmds[2]);
+    while (1)
+    {
+        if (take_line(inputString))
+            continue ;
+    //pipes = ft_split(inputString, '|');
+        cmds = parse_cmds(inputString);
+    // while (pipes[i])
+    // {
+    //     if (pipes[i])
+    //         pipes[i] = ft_strtrim(pipes[i], "\t ");
+    // }
+        exce_args(cmds);
+    }
 }
