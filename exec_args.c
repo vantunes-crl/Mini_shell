631f4d2fc72e3b5cdf02955d528e987f42e7b0d5
Valerio Antunes de souza junior <vantunes@c1r9s1.42lisboa.com>
2021-06-12 15:36:35 +0100

    working in multiples pipes

--- exec_args.c
+++ exec_args.c
@@ -29,7 +29,7 @@
 //     if (pid1 < 0)
 //         error("fork");
 
-//     child process 1
+//     //child process 1
 //     if (pid1 == 0)
 //     {
 //         dup2(fd[1], STDOUT_FILENO);
@@ -42,7 +42,7 @@
 //     if (pid2 < 0)
 //         error("fork");
 
-//     child process 2
+//     //child process 2
 //     if (pid2 == 0)
 //     {
 //         dup2(fd[0], STDIN_FILENO);
@@ -56,57 +56,84 @@
 //     wait(NULL);
 // }
 
-// void exec_multiple_pipes(char *str , int pipes)
-// {
-//     int **fds;
-//     int i = 0;
-
-//     fds = ft_calloc(pipes, sizeof(int));
-//     while (i < pipes)
-//     {
-//         fds[i] = ft_calloc(2, sizeof(int));
-//         i++;
-//     }
-//     i = 0;
-//     while (i < pipes)
-//     {
-//         if (pipe(fds[i]) < 0)
-//             error("pipe");
-//         i++;
-//     }
-
-//     pid_t pid1;
-//     pid1 = fork();
-
-//     if (pid1 == 0)
-//     {
-//         close(fds[0][0]);
-//         close(fds[0][1]);
-//         close(fds[1][0]);
-//         close(fds[1][1]);
-//         close(fds[2][0]);
-//         close(fds[2][1]);
-//         int x;
-//         if (read(fds[0][0], &x, sizeof(int)) < 0)
-//             return 3;
-//         x += 5;
-//         if (write(fds[1][1], &x, sizeof(int)) < 0)
-//             return 4;
-//         return (0);
-//     }
-// }
-// 0 0
-// 0 1
-// 1 0
-// 1 1
-// 2 0
-// 2 1
-int main()
+int exec_multiple_pipes(char *str , int pipes)
 {
+    int **fds;
     int i = 0;
-    int j;
 
-    while (i <= 2)
+    fds = ft_calloc(pipes, sizeof(int));
+    while (i < pipes)
+    {
+        fds[i] = ft_calloc(2, sizeof(int));
+        i++;
+    }
+    i = 0;
+    while (i < pipes)
+    {
+        if (pipe(fds[i]) < 0)
+            perror("pipe");
+        i++;
+    }
+
+    pid_t pid1;
+    pid1 = fork();
+
+    //child process 1
+    if (pid1 == 0)
+    {
+        int x;
+        int j;
+        if (read(fds[0][0], &x, sizeof(int)) < 0)
+            return 3;
+        x += 5;
+        if (write(fds[1][1], &x, sizeof(int)) < 0)
+            return 4;
+        int i = 0;
+        while (i <= pipes)
+        {
+            j = 0;
+            while (j < 2)
+            {
+                close(fds[i][j]);
+                j++;
+            }
+            i++;
+        }
+        return (0);
+    }
+
+    pid_t pid2;
+    pid2 = fork();
+
+    //child process 2
+    if (pid2 == 0)
+    {
+        int x;
+        int j;
+        if (read(fds[1][0], &x, sizeof(int)) < 0)
+            return 3;
+        x += 5;
+        if (write(fds[2][1], &x, sizeof(int)) < 0)
+            return 4;
+        int i = 0;
+        while (i <= pipes)
+        {
+            j = 0;
+            while (j < 2)
+            {
+                close(fds[i][j]);
+                j++;
+            }
+            i++;
+        }
+        return (0);
+    }
+    int x = 0;
+    if (write(fds[0][1], &x, sizeof(int)) < 0)
+        return 4;
+    int j;
+    i = 0;
+    while (i <= pipes)
     {
         j = 0;
         while (j < 2)
@@ -116,5 +143,18 @@ int main()
         }
         i++;
     }
-    //exec_multiple_pipes("abc | abc | abc", has_pipes("abc | abc | abc"));
+    printf("%d\n", x);
+    return (0);
+}
+// 0 0
+// 0 1
+// 1 0
+// 1 1
+// 2 0
+// 2 1
+int main()
+{
+
+    //printf("%d\n",has_pipes("ola | ola | ola"));
+    exec_multiple_pipes("abc | abc | abc", has_pipes("abc | abc | abc"));
 }
