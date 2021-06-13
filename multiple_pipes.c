4b3bbef0ed0139e2776d32fb02d7f9e8354152e3
Valerio Antunes de souza junior <vantunes@c1r10s1.42lisboa.com>
2021-06-13 17:32:56 +0100

    working in multiple pipes

--- multiple_pipes.c
+++ multiple_pipes.c
@@ -1,10 +1,25 @@
-
 #include "mini_shell.h"
+
+void close_fds(int pipes, int **fd)
+{
+    int i = 0;
+    int j;
+
+    while (i < pipes)
+    {
+        j = 0;
+        while (j < 2)
+        {
+            close(fd[i][j]);
+            j++;
+        }
+    i++;
+    }
+}
 
 void multiple_pipes(int pipes)
 {
     pid_t pid;
-    pid_t pid2;
     int i = -1;
     int **fd;
 
@@ -14,48 +29,33 @@ void multiple_pipes(int pipes)
     i = -1;
     while (++i < pipes)
         pipe(fd[i]);
-    pid = fork();
 
+    pid = fork();
     if (pid == 0)
     {   
         dup2(fd[0][1], 1); // cpy from stdout to the first pipe
-        close(fd[0][1]);
-        close(fd[0][0]);
-        close(fd[1][0]);
-        close(fd[1][1]);
+        close_fds(pipes, fd);
         exce_arg(parse_cmds("ls -l"));
     }
 
-    pid2 = fork();
-    if (pid2 == 0)
+    pid = fork();
+    if (pid == 0)
     {   
         dup2(fd[0][0], 0); // cpy from stdin to the first pipe
         dup2(fd[1][1],1); // cpy from pipe1 to pipe2
-        close(fd[0][1]);
-        close(fd[0][0]);
-        close(fd[1][1]);
-        close(fd[1][0]);
+        close_fds(pipes, fd);
         exce_arg(parse_cmds("ls -l"));
     }
 
-    pid_t pid3;
-
-    pid3 = fork();
-    if (pid3 == 0)
+    pid = fork();
+    if (pid == 0)
     {
         dup2(fd[1][0],0); //cpy from pipe1 to pipe2
-        close(fd[0][1]);
-        close(fd[0][0]);
-        close(fd[1][1]);
-        close(fd[1][0]);
+        close_fds(pipes, fd);
         exce_arg(parse_cmds("cat -e"));
     }
-
-    close(fd[0][1]);
-    close(fd[0][0]);
-    close(fd[1][1]);
-    close(fd[1][0]);
-    wait(NULL);
-    wait(NULL);
-    wait(NULL);
+    close_fds(pipes, fd);
+    i = -1;
+    while (++i <= pipes)
+        wait(NULL);
 }
