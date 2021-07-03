048d0ddf7f41accfb963eb72fa1676f106666259
Valerio Antunes de souza junior <vantunes@c2r8s4.42lisboa.com>
2021-07-03 16:29:20 +0100

    fix leaks

--- simple_redi.c
+++ simple_redi.c
@@ -3,15 +3,25 @@
 char *find_filename(char *cmd)
 {
     char **cmd_list;
+    char *temp;
+    int i;
 
+    i = 0;
     cmd_list = ft_split(cmd, ' ');
     if (cmd[0] == '<')
-        return(cmd_list[1]);
+    {
+        temp = ft_strdup(cmd_list[1]);
+        free_paths(cmd_list);
+        return(temp);
+    }
     else
     {
         while (*(cmd_list + 1))
             cmd_list++;
-        return(*cmd_list);
+        while (cmd_list[i] != NULL)
+            free(cmd_list[i++]);
+        temp = ft_strdup(*cmd_list);
+        return(temp);
     }
 }
 
@@ -19,7 +29,11 @@ char *new_cmd_in(char *cmd)
 {
     char **cmd_list;
     char *new_str;
+    char *temp;
+    char temp2;
+    int i;
 
+    i = 0;
     new_str = ft_strdup("");
     cmd_list = ft_split(cmd, ' ');
     if (cmd[0] == '<')
@@ -28,9 +42,13 @@ char *new_cmd_in(char *cmd)
         cmd_list++;
         while (*cmd_list)
         {
+            temp = new_str;
             new_str = ft_super_strjoin(3, new_str, " ", *cmd_list);
+            free(temp);
             cmd_list++;
         }
+        while (cmd_list[i] != NULL)
+            free(cmd_list[i++]);
         return (new_str);
     }
     else
@@ -39,9 +57,13 @@ char *new_cmd_in(char *cmd)
         {
             if (ft_strncmp("<", *cmd_list, 1) == 0)
                 break;
+            temp = new_str;
             new_str = ft_super_strjoin(3, new_str, " ", *cmd_list);
+            free(temp);
             cmd_list++;
         }
+        while (cmd_list[i] != NULL)
+            free(cmd_list[i++]);
         return (new_str);
     }
     return (NULL);
@@ -56,13 +78,17 @@ void simple_redirec_in(char *cmd, t_list **env, char **paths)
     char *new_cmd;
     char **temp_cmd;
 
-    pipe(fd);
+    if (pipe(fd) < 0)
+        perror("pipe");
     fn = find_filename(cmd);
     new_cmd = new_cmd_in(cmd);
     pid = fork();
+    if (pid < 0)
+        perror("fork");
     if (pid == 0)
     {
         fd_in = open(fn, O_RDWR, 0777);
+        free(fn);
         if (fd_in < 0)
         {
             perror("minishell");
@@ -72,6 +98,7 @@ void simple_redirec_in(char *cmd, t_list **env, char **paths)
         close(fd[0]);
         close(fd[1]);
         temp_cmd = parse_cmds(new_cmd);
+        free(new_cmd);
         exec_cmd(temp_cmd, env, paths);
         exit(0);
     }
