b58eae28e4f2c7199cd84f394c6d842e3b318b14
Valerio Antunes de souza junior <vantunes@c2r8s3.42lisboa.com>
2021-07-06 14:08:38 +0100

    norm

--- mini_shell.c
+++ mini_shell.c
@@ -12,16 +12,54 @@ t_list *init_env(char **env)
     return (elem);
 }
 
+void ft_cd(char **temp_dir, char **temp_cmds)
+{
+    temp_dir = ft_split(temp_cmds[0], ' ');
+    chdir(temp_dir[1]);
+    free_paths(temp_dir);
+    free_paths(temp_cmds);
+}
+
+void ft_unset(char **temp_dir, t_list *envp, char **temp_cmds)
+{
+    temp_dir = ft_split(temp_cmds[0], ' ');
+    del_elem_lst(&envp, temp_dir[1]);
+    free_paths(temp_dir);
+    free_paths(temp_cmds);
+}
+
+void main_loop(char **temp_cmds, t_list *envp)
+{   
+    char **temp_dir;
+    char **paths;
+
+    if (ft_strncmp(temp_cmds[0],"cd", 2) == 0)
+        ft_cd(temp_dir, temp_cmds);
+    else if (ft_strncmp(temp_cmds[0], "unset", 5) == 0)
+        ft_unset(temp_dir, envp, temp_cmds);
+    else if (ft_strncmp(temp_cmds[0], "export", 6) == 0)
+    {
+        temp_dir = ft_split(temp_cmds[0], ' ');
+        ft_lstadd_back(&envp, ft_lstnew(temp_dir[1]));
+        free_paths(temp_cmds);
+    }
+    else
+    {
+        has_exit(temp_cmds , &envp);
+        paths = find_path(temp_cmds, envp);
+        main_process(temp_cmds, &envp, paths);
+        free_paths(temp_cmds);
+    }
+}
+
 int main(int argc, char **argv, char **env)
 {
     char inputString[200];
     char **temp_cmds;
-    char **temp_dir;
     t_list *envp;
-    exit_status = 0;
-    char **paths;
-    envp = init_env(env);
 
+    exit_status = 0;
+    envp = init_env(env);
     signal(SIGINT, kill_handler);
 
     while (TRUE)
@@ -31,33 +69,7 @@ int main(int argc, char **argv, char **env)
         else
         {
             temp_cmds = cmds_list(inputString);
-            if (ft_strncmp(temp_cmds[0],"cd", 2) == 0)
-            {
-                temp_dir = ft_split(temp_cmds[0], ' ');
-                chdir(temp_dir[1]);
-                free_paths(temp_dir);
-                free_paths(temp_cmds);
-            }
-            else if (ft_strncmp(temp_cmds[0], "unset", 5) == 0)
-            {
-                temp_dir = ft_split(temp_cmds[0], ' ');
-                del_elem_lst(&envp, temp_dir[1]);
-                free_paths(temp_dir);
-                free_paths(temp_cmds);
-            }
-            else if (ft_strncmp(temp_cmds[0], "export", 6) == 0)
-            {
-                temp_dir = ft_split(temp_cmds[0], ' ');
-                ft_lstadd_back(&envp, ft_lstnew(temp_dir[1]));
-                free_paths(temp_cmds);
-            }
-            else
-            {
-                has_exit(temp_cmds , &envp);
-                paths = find_path(temp_cmds, envp);
-                main_process(temp_cmds, &envp, paths);
-                free_paths(temp_cmds);
-           }
+            main_loop(temp_cmds, envp);
         }
     }
     return (0);
